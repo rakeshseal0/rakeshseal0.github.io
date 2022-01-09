67dbaf08f08d281d800e413d8076904be1e2c05f
rakeshseal0 <rakeshseal0@gmail.com>
2022-01-09 18:17:34 +0530

    new version of portfolio.

---
+++ precache-manifest.ce0614e75cc34a2eec29fde2ef33bd49.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadf9ccdd2631c88a6deface5aab2914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cac06584c3f96fdffb8",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "fc743e1cf3d5b9715e96",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "0a4858f010a883347d2b",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "903ad2a25d04ca41ab2a",
+    "url": "/static/js/2.f3065902.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.f3065902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cac06584c3f96fdffb8",
+    "url": "/static/js/3.8f67c412.chunk.js"
+  },
+  {
+    "revision": "fc743e1cf3d5b9715e96",
+    "url": "/static/js/4.20b3dc7c.chunk.js"
+  },
+  {
+    "revision": "0a4858f010a883347d2b",
+    "url": "/static/js/main.1bbbffb7.chunk.js"
+  },
+  {
+    "revision": "acbbcbedd475f8be9959",
+    "url": "/static/js/runtime-main.6c8d00a9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/static/media/coursera.3952f41b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "7bad3695a55b43e68f4e734fd2070e4b",
+    "url": "/static/media/iemaward.7bad3695.png"
+  },
+  {
+    "revision": "ff27700a1a253f46d3a2f8ae1402f240",
+    "url": "/static/media/iemlogo.ff27700a.png"
+  },
+  {
+    "revision": "214a8f2be51edfb82b5f6fcaff0be31c",
+    "url": "/static/media/keysight-technologies--big.214a8f2b.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "691fdc4c2a8facf768a54eebbbf0cbb5",
+    "url": "/static/media/rodologo.691fdc4c.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
